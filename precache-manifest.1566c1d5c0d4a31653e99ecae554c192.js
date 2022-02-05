6e27d53cdff40bd360f3e256441bcaae3ed9f8f5
Zack <zckhtln@icloud.com>
2022-02-05 10:23:07 +0300

    Updates

---
+++ precache-manifest.1566c1d5c0d4a31653e99ecae554c192.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fb0476fc86b71f5fa114952e279378e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af9743d6e9f4ccd9ef96",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c739f0a9821fcb90f4f9",
+    "url": "/static/js/2.16586007.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.16586007.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0427cffb239dfe1c38b",
+    "url": "/static/js/3.e4676d59.chunk.js"
+  },
+  {
+    "revision": "af9743d6e9f4ccd9ef96",
+    "url": "/static/js/main.b416f0f1.chunk.js"
+  },
+  {
+    "revision": "848ff046d45e54953129",
+    "url": "/static/js/runtime-main.7f48b9bf.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  }
+]);
