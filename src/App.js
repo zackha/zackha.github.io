4754c5eb4c130e38436563f701927f9806f6e278
Zack <zckhtln@icloud.com>
2022-02-05 12:08:54 +0300

    Changing message

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hu <code>src/App.js</code> and save to reload.
+          huuuuu
         </p>
         <a
           className="App-link"
