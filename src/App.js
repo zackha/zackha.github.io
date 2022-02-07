c8f539bffe44ef59b125c6c531e3ed47056087df
Zack <zckhtln@icloud.com>
2022-02-07 18:40:10 +0300

    Changing message

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>In the name of God, the Lord of Mercy, the Giver of Mercy!</h1>
         <p>
-          testing
+          Do not lose heart or despair- if you are true believers you have the upper hand-
         </p>
       </header>
     </div>
