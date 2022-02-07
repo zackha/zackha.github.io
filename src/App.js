a7fcbbc4e19a8648c1d5fba862b0ffb5755ed4ee
Zack <zckhtln@icloud.com>
2022-02-07 18:43:13 +0300

    Changing title

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>In the name of God, the Lord of Mercy, the Giver of Mercy!</h1>
         <p>
           Do not lose heart or despair- if you are true believers you have the upper hand-
         </p>
